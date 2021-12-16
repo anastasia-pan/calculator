5f4bf5673c1287e78ea9a19428174cc9dd91ff53
Anastasia Panoutsou <panoutsou.anastasia@gmail.com>
2021-12-16 22:25:31 +0000

    initial commit, it works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { columnTransformDependencies, evaluate } from "mathjs";
 
-function App() {
+const Calculator = () => {
+  const operators = ["+", "-", "/", "x", "%"];
+
+  const numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "."];
+
+  //   "=",
+  //   "AC",
+
+  const [userInput, setUserInput] = useState("");
+  const [userTotal, setUserTotal] = useState([]);
+
+  const getValue = (value) => {
+    let x = `${userInput}${value}`;
+    setUserInput(x);
+  };
+  const getTotal = () => {
+    console.log(userInput);
+    let total = evaluate(userInput);
+    setUserInput(total);
+  };
+
+  const clearInput = () => {
+    setUserInput("");
+  };
+
+  const playClick = () => {
+    const click = "./sounds/click";
+    click.play();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wide">
+      <div className="calculator">
+        <h1 className="screen">{userInput}</h1>
+        <div className="numbers">
+          {numbers.map((number, index) => (
+            <Button key={index} number={number} getValue={getValue} />
+          ))}
+        </div>
+        <div className="operators">
+          {operators.map((operator, index) => (
+            <Button key={index} number={operator} getValue={getValue} />
+          ))}
+          <button onClick={getTotal}> = </button>
+          <button onClick={clearInput}> AC </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+const Button = (props) => {
+  const handleClick = (event) => {
+    props.getValue(event.target.value);
+  };
+
+  return (
+    <>
+      <button value={props.number} onClick={handleClick}>
+        {props.number}
+      </button>
+    </>
+  );
+};
+
+const Head = (props) => {
+  return <></>;
+};
+
+export default Calculator;
